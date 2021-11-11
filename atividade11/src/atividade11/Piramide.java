69b4f71b27b34c1def9cee60b48172c93e159a42
kalildeoliveira <69366429+kalildeoliveira@users.noreply.github.com>
2021-11-11 10:57:13 -0300

    Add files via upload

---
+++ atividade11/src/atividade11/Piramide.java
@@ -0,0 +1,105 @@
+package atividade11;
+
+import java.util.Scanner;
+
+public class Piramide {
+	Scanner teclado = new Scanner(System.in);
+	private double area;
+	private double altura;
+	private double base;
+	private double volume;
+	private double valorTinta;
+	
+	
+// METODOS PÚBLICOS GET E SET
+	public double getArea() {
+		return area;
+	}
+
+	public void setArea(double area) {
+		this.area = area;
+	}
+
+	public double getAltura() {
+		return altura;
+	}
+
+	public void setAltura(double altura) {
+		this.altura = altura;
+	}
+
+	public double getBase() {
+		return base;
+	}
+
+	public void setBase(double base) {
+		this.base = base;
+	}
+
+	public double getVolume() {
+		return volume;
+	}
+
+	public void setVolume(double volume) {
+		this.volume = volume;
+	}
+	public double getValorTinta() {
+		return valorTinta;
+	}
+
+	public void setValorTinta(double valorTinta) {
+		this.valorTinta = valorTinta;
+	}
+
+
+// MÉTODO PÚBLICO TO STRING
+	@Override
+	public String toString() {
+		return "O volume do seu triângulo é = " + volume;
+	}
+	
+// OUTROS MÉTODOS
+	public void telaInicial() {
+		System.out.println("****************************************************\n\n"
+				+ "\tSeja bem-vindo ao nosso sistema!\n\n"
+				+ "\tPara mais informações, entre em contato: \n"
+				+ "\tkalil.fadesc@gmail.com\n\n \t\tMUITO OBRIGADO! \n\n"				
+				+ "****************************************************");
+	}
+	
+	public void escolherTinta() {		
+		String tinta1="1", tinta2="2", tinta3="3";
+		System.out.println("Informe o tipo de Tinta");
+		System.out.println("\"1\" - 127,90");
+		System.out.println("\"2\" - 258,98");
+		System.out.println("\"3\" - 344,34");
+		System.out.print("Opcão: ");
+		String opcao = teclado.next();
+		if (opcao.equals(tinta1)) {
+			setValorTinta(127.90);
+		} else if (opcao.equals(tinta2)) {
+			setValorTinta(258.98);
+		} else if (opcao.equals(tinta3)) {
+			setValorTinta(344.34);
+		} else {
+			System.out.println("Dados incorretos. Tente novamente \n\n");
+			escolherTinta();
+		}
+		
+	}
+	public void calcularVolume() {		
+		System.out.println("Informe os dados do Triângulo");
+		System.out.print("Base: ");		
+		double base = teclado.nextDouble();
+		setBase (base);
+		System.out.print("Altura: ");
+		double altura = teclado.nextDouble();
+		setAltura (altura);
+		double area = base*altura;
+		setArea (area);
+		double volume = (base/3)*altura; 
+		setVolume(volume);		
+	}
+	
+
+}
---
+++ atividade11/src/atividade11/ProgramaPrincipalPiramide.java
@@ -0,0 +1,15 @@
+package atividade11;
+
+public class ProgramaPrincipalPiramide {
+
+	public static void main(String[] args) {
+		Piramide p = new Piramide();
+//		p.telaInicial();
+//		p.escolherTinta();
+//		p.calcularVolume();
+		p.toString();
+		
+
+	}
+
+}
